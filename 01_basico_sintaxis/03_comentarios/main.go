23f5634fc703748c81ba151ec2a9d6f8a5ed1520
pystudent1913 <zcristian1913@hotmail.com>
2018-04-12 23:57:00 -0500

    convention

--- 01_basico_sintaxis/03_comentarios/main.go
+++ 01_basico_sintaxis/03_comentarios/main.go
@@ -10,4 +10,7 @@ func main() {
 		  Bloque
 	*/
 	fmt.Println("Y tienen significado sintactico")
+	// importante para comentar tu codigo a modo de
+	// explicacion la convencion es usar solo este tipo
+	// de comentarios de una linea
 }
