fc25a1721860f140ef13fde9fa385d74361dea0c
pystudent1913 <zcristian1913@hotmail.com>
2018-04-16 01:54:50 -0500

    tipos de datos

---
+++ 01_basico_sintaxis/04_tipos_de_datos/main.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+  importamos reflect para llamar a su metodo
+  TypeOf() para inspeccionar el tipo de las variables
+*/
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// y aqui podemos probar pasandole valores
+	// a variables y que go detecte que tipo de datos son
+	nombre := "cristian"
+	numeroEntero := 123
+	numeroDecimal := 10.41
+	booleano := false
+
+	fmt.Println(reflect.TypeOf(nombre))
+	fmt.Println(reflect.TypeOf(numeroEntero))
+	fmt.Println(reflect.TypeOf(numeroDecimal))
+	fmt.Println(reflect.TypeOf(booleano))
+	// forma alternativa
+	fmt.Printf("nombres es de tipo: %T\n", nombre)
+	fmt.Printf("nombres es de tipo: %T\n", numeroEntero)
+	fmt.Printf("nombres es de tipo: %T\n", numeroDecimal)
+	fmt.Printf("nombres es de tipo: %T\n", booleano)
+	// valor cero
+	var nombre1 string
+	// nombre = ""
+	// fmt.Println(nombre) -> retorna linea en blanco
+	var numero int
+	// numero = 0
+	var entiendes boolean
+	// entiendes = false OJO!!!!!
+}
