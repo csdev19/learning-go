aff721bc9cfb824473186ab9c8ef0e81eba724d4
pystudent1913 <zcristian1913@hotmail.com>
2018-05-09 22:58:55 -0500

    interfaces2

---
+++ 10_interfaces/details.md
@@ -0,0 +1,6 @@
+# Interfaces
+
+- **GO** al igual que otros lenguajes de programacion orientado a objetos, cuenta con interfaces. Y estas son funciones que pueden ser implementadas dentro de cualquier **objeto** aunque en el caso de **GO** estos pueden ser implementados en cualquier tipo de dato.
+- El ejemplo entero esta en **main.go**. Al usar figuras nos podemos dar cuenta que todas tiene un **area** y un **perimetro** entonces para que el codigo sea mas optimo y reusable creariamos interfaces de **area** y **perimetro**.
+- Las interfaces en **GO** nos permiten enlazar funciones independientemente del tipo de dato que mandemos.
+
--- 10_interfaces/main.go
+++ 10_interfaces/main.go
@@ -1,8 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+/*
+	Aqui pondremos un ejemplo de interfaces con figuras
+	geometricas. AREA Y PERIMETROS
+*/
+
+// Estructura e interface CIRCULO
+type Circulo struct {
+	radio float64
+}
+
+func (c Circulo) area() (resultado float64) {
+	resultado = math.Pi * (c.radio * c.radio)
+	return resultado
+}
+
+func (c Circulo) perimetro() (resultado float64) {
+	resultado = math.Pi * c.radio * 2
+	return resultado
+}
+
+// Estructura e interface CUADRADO
+type Cuadrado struct {
+	lado float64
+}
+
+func (c Cuadrado) area() (resultado float64) {
+	resultado = c.lado * c.lado
+	return resultado
+}
+
+func (c Cuadrado) perimetro() (resultado float64) {
+	resultado = c.lado * 4
+	return resultado
+}
+
+// Estructura e interface RECTANGULO
+type Rectangulo struct {
+	base   float64
+	altura float64
+}
+
+func (r Rectangulo) area() (resultado float64) {
+	resultado = r.base * r.altura
+	return resultado
+}
+
+func (r Rectangulo) perimetro() (resultado float64) {
+	resultado = r.base*2 + r.altura
+	return resultado
+}
+
+// Interface Figuras
+type Figuras interface {
+	area() float64
+	perimetro() float64
+}
 
 func main() {
 	// Interfaces
-	fmt.Println()
+	circulo := Circulo{10.0}
+	cuadrado := Cuadrado{10}
+	rectangulo := Rectangulo{5.0, 2.0}
+
+	area2 := area(rectangulo)
+	area := area(cuadrado)
+	perimetro := perimetro(circulo)
+	fmt.Println("el area es: ", area)          // 100
+	fmt.Println("el area es: ", area2)         // 100
+	fmt.Println("el perimetro es:", perimetro) // 62,..
+}
+
+func area(f Figuras) float64 {
+	return f.area()
+}
+
+func perimetro(f Figuras) float64 {
+	return f.perimetro()
 }
